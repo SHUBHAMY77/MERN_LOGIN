01921d42a88a16a9ab5fd65fa4059877eaccaa78
shubhamry <shubham.yajurvedi@vamstar.io>
2022-10-25 22:36:48 +0530

    mongodb connect test code

--- server/index.js
+++ server/index.js
@@ -1,11 +1,29 @@
 const express = require("express")
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+
+dotenv.config();
+
+
+
+//setup a server
 const app = express()
-
-
 const PORT = process.env.PORT ||  5000;
-
 app.listen(PORT,()=>{console.log(`website is running on port:- ${PORT}`)})
 
-app.get("/test",(req,res)=>{
-    res.send("it Works ")
+
+
+//setup a dataBase
+
+mongoose.connect(process.env.MDB_CONNECT,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+},(err)=>{
+    if(err) return  console.log(err);
+    console.log("connected to dataBase")
 })
+
+
+//VPOInpYksqT0aUQA
+
+//mongodb+srv://shubham96:<password>@cluster0.aareobm.mongodb.net/?retryWrites=true&w=majority
